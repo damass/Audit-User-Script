aeb0ca6af67b65a552ea2c553324640ad6216338
David Mass <david.mass@dynatrace.com>
2019-09-17 11:25:18 -0400

    Upload v2.1

--- auditUsers.py
+++ auditUsers.py
@@ -2,9 +2,11 @@
 #
 #	Name		: auditUsers.py
 #	Creation Date 	: 2018-10-15
-#	Version 	: 1.0 
+#	Update : 2019-08-10
+#	Version 	: 2.1 
 #	
-#	Requires Python3
+#	Requires : Python3
+#			   Dynatrace Managed v158+
 #
 #	Use : Place auditUsers.py in the same directory as the audit.user.0.0.log 
 #		 and run the script. It will create a results directory with the 
@@ -15,16 +17,17 @@
 #		2.) Unique User logins per day - uniqueUsersPerDay.txt & uniqueSuccessGraph.csv
 #		3.) Failed User Logins by day - failureList.csv
 #		4.) List of Unique Users - summary.txt
+#		5.) Lists number of logins per user - loginsPerUser.csv
 #
 ####################################################################################################
 
 import os
 import sys
 import time
+import json
+import logging
 
-inputFileName = "audit.user.0.0.log"
-
-
+inputFileName = "full.audit.user.0.0.log"
 
 totalAttempts = 0
 totalSuccesses = 0
@@ -32,25 +35,34 @@ totalFailures = 0
 totalSuccessDict = dict() #total successful logins per day
 uniqueSuccessArr = [] # unique successful logins per day 
 uniqueUserList = [] # holds the date and unique users, gets cleared for each date 
+loginsPerUser = dict() # holds number of logins for each user 
 earliestDate = "01/01/1970"
 lastDate = "01/01/1970"
+strDateTime = str(time.strftime("%Y%m%d_%H%M%S",time.localtime())) #date&time for output files
+isDevOps = True # used to remove DevOps access to the Web UI through Mission Control 
 
+#set up logging 
+logFileName = "audit-user-logs-log."+strDateTime+".log"
+#logging.basicConfig(filename=logFileName,level=logging.DEBUG)
+logging.basicConfig(filename=logFileName,level=logging.INFO)
+logging.info('Log File Open')
+
+logging.info('Create Results Folder/Directory')
 try:
 	#find OS and create results directory for output files 
 	localFolder = os.path.abspath("")
 	OSType = sys.platform 
 	if str(OSType) != "win32":
-		resultsFolder = localFolder+'/results/'
+		resultsFolder = localFolder+'/audit-user-results/'
 		os.mkdir(resultsFolder)
 	else: 
-		resultsFolder = localFolder+'\\results\\'
+		resultsFolder = localFolder+'\\audit-user-results\\'
 		os.mkdir(resultsFolder)
 except:
-	print("results directory already created") 
+	logging.info("results directory already created") 
 
-strDateTime = str(time.strftime("%Y%m%d_%H%M%S",time.localtime()))
 
-	
+logging.info('Defining Output File Names')
 # Output File Names
 successList = os.path.join(resultsFolder,"successList-"+strDateTime+".csv") #For each successful Login Print - Date,UserName
 usersPerDay = os.path.join(resultsFolder,"uniqueUsersPerDay-"+strDateTime+".txt") #Prints list of unique usernames per day 
@@ -58,15 +70,42 @@ uniqueSuccessGraph = os.path.join(resultsFolder,"uniqueSuccessGraph-"+strDateTim
 failureList = os.path.join(resultsFolder,"failureList-"+strDateTime+".csv") #For each failed login print - Date,UserName 
 successGraph = os.path.join(resultsFolder,"successGraph-"+strDateTime+".csv") # prints Date, total # of logins for that day to be able to graph 
 summaryFile = os.path.join(resultsFolder,"summary-"+strDateTime+".txt") #Print Summary with totals and list of unique users
+loginsPerUserFile = os.path.join(resultsFolder,"loginsPerUser-"+strDateTime+".csv") #Print # of Logins for each user over course of Audit log file 
 
 
+logging.info('Main Loop Begin')
 with open(inputFileName) as inputFile: #Open & Read in each line of the Audit User Log
 	#begin main loop
+	logging.debug('Audit Log File Open')
 	for line in inputFile:
 		linePieces = line.strip()
-		linePieces = linePieces.split() #split the incoming line on a "space" character
-		if linePieces[3] == "WebUI" and linePieces[4] == "Login": # If someone tries to login 
-			if linePieces[5] != "failed":	# and it sucessful
+		logging.debug('Splitting Line')
+		linePieces = linePieces.split(" ", 3) #split the incoming line on a "space" character 3 times to keep full JSON string
+		
+		logging.debug('Testing to see if Audit Logs contains non JSON Lines')
+		try:
+			json_object = json.loads(linePieces[3])
+		except ValueError as e:
+			logging.warning('Not all log lines are JSON Formatted')
+			sys.exit()
+		
+		#JSON Specific Log Lines 
+		logging.debug('Pulling out JSON String')
+		fullJSONString=linePieces[3] #capture full JSON string from split out line pieces
+		logging.debug(fullJSONString)
+		JSONLogDict = json.loads(fullJSONString) #turn FullJSONString into Python Dictionary
+		logging.debug('JSON Converted to Dictionary')
+		
+		#check if user is a DevOps user from Dynatrace
+		if "DevOpsToken" in JSONLogDict["userId"]:
+			isDevOps = True
+		else: 
+			isDevOps = False
+
+		if JSONLogDict["identityCategory"] == "WEB_UI" and JSONLogDict["eventType"] == "LOGIN" and JSONLogDict["userIdType"] == "USER_NAME" and isDevOps is False: # If a real user not using Mission Control logins in  
+			logging.debug('WebUI Login Attempt')
+			if JSONLogDict["success"] is True:	# and it sucessful
+				logging.debug('Successful WebUI Login')
 				totalSuccesses += 1 #Increment Counters
 				totalAttempts += 1
 				
@@ -77,22 +116,38 @@ with open(inputFileName) as inputFile: #Open & Read in each line of the Audit Us
 					strUpdatedValue = str(updatedValue)
 					updatedInfo = {linePieces[0]: strUpdatedValue}
 					totalSuccessDict.update(updatedInfo)
+					logging.debug('Subsquent Login for the day - '+linePieces[0])
 				#If the date doesn't exist, add it to the dictionary with an initial value of 1
 				else:
 					totalSuccessDict[linePieces[0]] = "1"
-								
+					logging.debug('First login of the day - '+linePieces[0])
 				#For each successful Login Print - Date and UserName
-				userName = linePieces[7].split(',')
+				userName = JSONLogDict["userId"]
 				outputSuccessFile = open(successList, "a")
-				outputSuccessFile.write(linePieces[0]+","+userName[0]+"\n") #print success
+				outputSuccessFile.write(linePieces[0]+","+userName+"\n") #print success
 				
+				logging.debug('Adding user to Unique User List')
 				#need to make sure usernames aren't repeated (John.Doe would be different than john.doe)
-				uniqueUser = userName[0].lower()
+				uniqueUser = userName.lower()
 				if uniqueUser not in uniqueUserList: #keeps a list of unique users to print out later
 					uniqueUserList.append(uniqueUser)
 				
+				logging.debug('Counting number of logins for each uniqueUser')
+				#keeps track of number of logins for each user over course of Audit Log File 
+				if uniqueUser in loginsPerUser:
+					updatedValue = int(loginsPerUser[uniqueUser]) + 1
+					strUpdatedValue = str(updatedValue)
+					updatedInfo = {uniqueUser: strUpdatedValue}
+					loginsPerUser.update(updatedInfo)
+					logging.debug('Subsequent Login for User')
+				else:
+					logging.debug('Adding User ' + uniqueUser + ' for First Login')
+					loginsPerUser[uniqueUser] = "1" 
+					logging.debug('First Login for User')
+					
 				# find number of unique logins per day 
 				#if date already in list and userName not it list, add user 
+				logging.debug('Find Unique Logins Per Day')
 				if(linePieces[0].strip() in uniqueSuccessArr) and (uniqueUser not in uniqueSuccessArr):
 					uniqueSuccessArr.append(uniqueUser)
 				#if the date is the next day, and there is already data in the list, write out the old data
@@ -121,9 +176,9 @@ with open(inputFileName) as inputFile: #Open & Read in each line of the Audit Us
 				totalFailures += 1
 				totalAttempts += 1
 				
-				userName = linePieces[8].split(',')
+				userName = JSONLogDict["userId"]
 				outputFailureFile = open(failureList, "a")
-				outputFailureFile.write(linePieces[0]+","+userName[0]+"\n") #or write failure
+				outputFailureFile.write(linePieces[0]+","+userName+","+JSONLogDict["message"]+"\n") #or write failure
 	#end main loop 
 				
 #print last date for unique user logins
@@ -135,12 +190,14 @@ outputUniqueLoginFile = open(uniqueSuccessGraph,"a")
 outputUniqueLoginFile.write(uniqueSuccessArr[0]+","+ str(len(uniqueSuccessArr) - 1)+"\n")
 				
 # after going through the file, print out number of successful logins per day 
-# if you open this up in excel, you can easily make a graph of total logins per day 				
+# if you open this up in excel, you can easily make a graph of total logins per day 
+logging.info('Writing Successful logins per day to CSV File')				
 for k, v in totalSuccessDict.items():
 	outputSuccessGraphFile = open(successGraph, "a")
 	outputSuccessGraphFile.write(k + "," + v + "\n")
 	
 #Print Summary with totals and list of unique users 
+logging.info('Writing out Summary Output File')
 outputSummaryFile = open(summaryFile, "a")
 outputSummaryFile.write("Total Attempts   	 : "+str(totalAttempts)+"\n")
 outputSummaryFile.write("Total Successful 	 : "+str(totalSuccesses)+"\n")
@@ -149,13 +206,22 @@ outputSummaryFile.write("Earliest Login Date : "+str(earliestDate)+"\n")
 outputSummaryFile.write("Last Login Date	 : "+str(lastDate)+"\n\n\n")
 outputSummaryFile.write("Unordered List of Unique Users : \n")
 
+logging.info('Writing all unique users to Summary Output File')
 for user in uniqueUserList:
 	outputSummaryFile.write(user+"\n")
 
+#writing out Logins per UserName to file : 
+for k, v in loginsPerUser.items():
+	outputloginsPerUserFile = open(loginsPerUserFile, "a")
+	outputloginsPerUserFile.write(k + "," + v + "\n")
+
 #close all files
+logging.info('Closing Output Files')
 outputSuccessFile.close()
 outputUniqueUsersPerDayFile.close()
 outputUniqueLoginFile.close()
 outputFailureFile.close()
 outputSuccessGraphFile.close()
 outputSummaryFile.close()
+outputloginsPerUserFile.close()
+logging.info('Script Exit Successfully') 
